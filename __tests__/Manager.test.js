41220534636467acc6291d30558539cb3a76c30d
BeatyPete <akira941@gmail.com>
2020-12-13 01:05:35 -0600

    manager test and constructor written and passing

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,11 @@
+const Manager = require('../lib/Manager.js');
+
+test('creates an Manager', () => {
+    const manager = new Manager('Dave', 19, 'dave@fakemail.com', '666-666-6666');
+  
+    expect(manager.name).toBe('Dave');
+    expect(manager.id).toEqual(19);
+    expect(manager.email).toBe('dave@fakemail.com');
+    expect(manager.phone).toBe('666-666-6666');
+    expect(manager.role).toBe('manager')
+});
